9fed0990e26e28f21555f4bd0c52bebe605c22ff
Perky219 <perky6929@gmail.com>
2025-11-18 21:16:52 -0600

    feat: add test wrapper for ProductosControl component jut for use the part the tests need

---
+++ src/Vistas/ProductosControlTestWrapper.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ProductosControl from "./ProductosControl.jsx";
+
+export default function ProductosControlTestWrapper({ productos }) {
+  return <ProductosControl productos={productos} />;
+}
